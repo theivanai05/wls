64ef57e06a12f697604cd3dcf87c88414c248845
Theivanai05 <theivanai05@gmail.com>
2020-08-21 13:43:12 +0800

    Test on First R file

---
+++ Main.r
@@ -0,0 +1,24 @@
+## Main File 
+
+## Installing all of the Libraries required : 
+
+  #install.packages("tidyverse")
+  #install.packages("data.table")
+  #install.packages("dplyr")
+  # -- Not Required #install.packages("Hmisc")
+
+  library(tidyverse)
+  library(data.table)
+  library(dplyr)
+  # -- Not Required #library(R.utils)
+  # -- Not Required #library(Hmisc)
+  
+## Setting Working Directory 
+  setwd("~/wls")
+  
+  ## New Data Directory 
+     # "~/wls/Data"
+  ## Code Directory 
+     # "~/wls/Code"
+  
+  
